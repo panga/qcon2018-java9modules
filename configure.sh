4b3514d65a997a90039c4d28e91037a30549b9e7
Leonardo Zanivan <panga@apache.org>
2018-05-07 19:12:13 -0300

    Add JVM opts

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 export JDK8_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export JDK10_HOME=/opt/java/jdk-10.0.1
 export JDK10_J9_HOME=/opt/java/jdk-10+46_j9
-export JVM_OPTS="-Xmx512m"
+export JVM_OPTS="-Xmx512m -XX:+UseG1GC"
 
 export JAR_NAME=spring-petclinic-2.0.1.jar
 export MODULE_NAME=org.springframework.samples.petclinic
