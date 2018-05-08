3c26a86b4555d17af39b9667fb99bd9b1f9c1013
Leonardo Zanivan <panga@apache.org>
2018-05-07 23:19:58 -0300

    Improve J9 startup

--- test-jdk10_j9.sh
+++ test-jdk10_j9.sh
@@ -4,4 +4,4 @@ source "./configure.sh"
 JAVA=$JDK10_J9_HOME/bin/java
 BASE_DIR=jdk10
 
-exec $JAVA $JVM_OPTS --add-modules java.xml.bind -jar $BASE_DIR/$JAR_NAME
+exec $JAVA $JVM_OPTS --add-modules java.xml.bind -Xquickstart -jar $BASE_DIR/$JAR_NAME
--- test-jdk10_j9_aot.sh
+++ test-jdk10_j9_aot.sh
@@ -4,4 +4,4 @@ source "./configure.sh"
 JAVA=$JDK10_J9_HOME/bin/java
 BASE_DIR=jdk10
 
-exec $JAVA $JVM_OPTS --add-modules java.xml.bind -XX:+IdleTuningGcOnIdle -Xtune:virtualized -Xscmx128m -Xscmaxaot100m -Xshareclasses:cacheDir=$BASE_DIR/shareclasses -cp "$BASE_DIR/modules/*:$BASE_DIR/upgrade-modules/*" $MAIN_CLASS
+exec $JAVA $JVM_OPTS --add-modules java.xml.bind -Xquickstart -Xscmx128m -Xscmaxaot100m -Xshareclasses:cacheDir=$BASE_DIR/shareclasses -cp "$BASE_DIR/modules/*:$BASE_DIR/upgrade-modules/*" $MAIN_CLASS
